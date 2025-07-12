34da02d86fcb4e7847358341acd6e11eb07a9849
Chanda Mounica <pvryar.4@gmail.com>
2025-07-12 08:11:11 +0530

    methods revision

---
+++ CSharpStep3/Methods Advanced.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Web;
+
+
+namespace Method_Advanced
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Subject { get; set; }
+
+
+    }
+
+    class StudentOperations
+    {
+        //public static Student GetStudent()
+        //{
+        //    Student student = new Student()
+        //    {
+        //        Name = "Doe",
+        //        Age = 25,
+        //        Subject = "Chemistry"
+        //    };
+        //    return student;
+        //}
+
+
+        // public static Student GetStudent()
+        //{
+        //    Student student = new Student();
+        //    student.Name = "John";
+        //        student.Age = 20;
+        //    student.Subject = "Maths";
+        //    return student;
+        //}
+
+
+        public static Student GetStudent()
+        {
+            return new Student()
+            {
+                Name = "Harry",
+                Age = 23,
+                Subject = "JAVA"
+            };
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Student singleStudent = StudentOperations.GetStudent();
+            Console.WriteLine($"single student:{singleStudent.Name}, {singleStudent.Age}, {singleStudent.Subject}");
+        }
+    }
+
+
+
+
+
+
+}
+
+
+
+
--- CSharpStep3/Program.cs
+++ CSharpStep3/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharpStep3
+namespace MathProgram
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static string[] ProcessArray(int[] numbers)
         {
+            //create a string array to store result
+            string[] result = new string[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    result[i] = $"number[i] is Even";
+                }
+                else
+                {
+                    result[i] = $"number[i] is Odd";
+                }
+            }
+            return result;
+
+        }
+
+        static void Main()
+        {
+            int[] numbers = { 1, 45, 64, 37, 48, 59 };
+            string[] output = ProcessArray(numbers);
+            Console.WriteLine(output);
+            foreach(string result in output)
+            {
+                Console.WriteLine(result);
+            }
+
         }
     }
 }
